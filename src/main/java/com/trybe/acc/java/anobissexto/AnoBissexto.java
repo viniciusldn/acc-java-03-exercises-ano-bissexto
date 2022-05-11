87c8f9e2a71ab0cd78f1e827bbd23b88a089b6de
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-05-11 15:13:32 -0300

    Initial commit

---
+++ src/main/java/com/trybe/acc/java/anobissexto/AnoBissexto.java
@@ -0,0 +1,12 @@
+package com.trybe.acc.java.anobissexto;
+
+public class AnoBissexto {
+	
+	public boolean identificarAnoBissexto(int ano) {
+	  return false;
+	}
+	
+	public int identificarProximoAnoBissexto(String data) {
+	  return 0;
+    }
+}
