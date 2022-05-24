7cf409e3d53259b25040e18000706e00e3f88f97
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-05-24 11:51:02 -0300

    Commit Inicial | [vinicius leite]-exercise-ano-bissexto

--- src/main/java/com/trybe/acc/java/anobissexto/AnoBissexto.java
+++ src/main/java/com/trybe/acc/java/anobissexto/AnoBissexto.java
@@ -1,12 +1,28 @@
 package com.trybe.acc.java.anobissexto;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class AnoBissexto.
+ */
 public class AnoBissexto {
-	
-	public boolean identificarAnoBissexto(int ano) {
-	  return false;
-	}
-	
-	public int identificarProximoAnoBissexto(String data) {
-	  return 0;
-    }
+
+  /**
+   * Identificar ano bissexto.
+   *
+   * @param ano the ano
+   * @return true, if successful
+   */
+  public boolean identificarAnoBissexto(int ano) {
+    return false;
+  }
+
+  /**
+   * Identificar proximo ano bissexto.
+   *
+   * @param data the data
+   * @return the int
+   */
+  public int identificarProximoAnoBissexto(String data) {
+    return 0;
+  }
 }
